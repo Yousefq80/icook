a938c614f62b57fd9eea25561e27d502130dfa2f
ranaBakri <75160099+ranaBakri@users.noreply.github.com>
2022-12-04 04:42:48 +0300

    I will fix it tomorrow with the instructors

---
+++ lib/SignIn.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class SignIn extends StatelessWidget {
+  SignIn({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  var formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign-In"),
+      ),
+      body: SafeArea(
+        child: Form(
+          key: formKey,
+          child: Column(children:[
+            TextFormField(
+              controller: usernameController,
+              decoration: InputDecoration(hintText: "Username"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Required field";
+                }
+
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(hintText: "Password"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Required field";
+                }
+
+                return null;
+              },
+            ),
+          ]),
+          // to insert button
+          Spacer(),
+            ElevatedButton(
+              onPressed: () async {
+                if (formKey.currentState!.validate()) {
+                  print("vlid");
+                } else {
+                  print("form not valid");
+                }
+              },
+              child: Text("Signup"),
+            ),
+          ])
+      ),
+        // ignore: dead_code
+        );
+      
+  }
+}
---
+++ lib/SignUp.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class SignUp extends StatelessWidget {
+  SignUp({super.key});
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  var formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign-up"),
+      ),
+      body: SafeArea(
+        child: Form(
+          key: formKey,
+          child: Column(children: [
+            TextFormField(
+              controller: usernameController,
+              decoration: InputDecoration(hintText: "Username"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Required field";
+                }
+
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(hintText: "Password"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Required field";
+                }
+
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: confirmPasswordController,
+              obscureText: true,
+              decoration: InputDecoration(hintText: "Confirm Password"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Required field";
+                }
+
+                if (passwordController.text != value) {
+                  return "Password doesn't match";
+                }
+
+                return null;
+              },
+            ),
+            // to insert button
+            Spacer(),
+            ElevatedButton(
+              onPressed: () async {
+                if (formKey.currentState!.validate()) {
+                  print("vlid");
+                } else {
+                  print("form not valid");
+                }
+              },
+              child: Text("Signup"),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
